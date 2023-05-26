fd5fb3624126e354d90fb30db50a8f005a08d7cf
Shirley <shirleyzhu@college.harvard.edu>
2023-05-26 16:52:41 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by ZhuMacPro on 5/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
